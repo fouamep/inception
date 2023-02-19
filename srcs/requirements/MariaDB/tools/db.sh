161b5c3b4e17b3fe074ab519ff7c684918d01b78
Fouad Bouanane <fbouanan@e2r9p4.1337.ma>
2023-02-19 18:09:42 +0100

    just check and push

--- srcs/requirements/MariaDB/tools/db.sh
+++ srcs/requirements/MariaDB/tools/db.sh
@@ -3,8 +3,8 @@
 service mysql start
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
