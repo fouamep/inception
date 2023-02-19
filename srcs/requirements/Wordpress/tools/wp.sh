69d69533a2d60cbf5f0d2ad687fc5d8ed7ea50c4
Fouad Bouanane <fbouanan@e2r9p4.1337.ma>
2023-02-19 18:52:09 +0100

    edit .env

--- srcs/requirements/Wordpress/tools/wp.sh
+++ srcs/requirements/Wordpress/tools/wp.sh
@@ -13,8 +13,7 @@ mv /wp-config.php /var/www/html/wp-config.php
 sed -i -r "s/db1/$DB_NAME/1"   wp-config.php
 sed -i -r "s/user/$MYSQL_USER/1"  wp-config.php
 sed -i -r "s/pwd/$DB_PASS/1"    wp-config.php
-sed -i -r "s/mariadb/$DB_HOST/1"    wp-config.php
-wp core install --url=$DOMAIN_NAME --title=simple_title --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=fouadbouanane6@gmail.com --skip-email --allow-root
+wp core install --url=fbouanan.42.fr --title=simple_title --admin_user=fouad --admin_password=fouad123--admin_email=fouadbouanane6@gmail.com --skip-email --allow-root
 wp user create user fouadbouanane@gmail.com --role=author --user_pass=fbouanan --allow-root
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 /usr/sbin/php-fpm7.3 -F -R
